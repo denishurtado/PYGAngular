ee96c6b6497dfa01c710966ba7287f7568aa612a
Denis Hurtado <denhurta@crsjopgp6069.global.publicisgroupe.net>
2017-01-06 16:08:48 -0600

    crud work

--- src/scripts/controllers/FormCtrl.js
+++ src/scripts/controllers/FormCtrl.js
@@ -9,20 +9,48 @@ var FormCtrl = function($scope, $http){
 	function getMessage(){
 
 		$http.get('http://localhost:5000/api/message').then(function(result){
-			console.log(result.data);
 			$scope.messages = result.data;	 
 		});
 	}
 	getMessage();
 
+	//get by id
+	function getMessageById(){
+
+		$http.get('http://localhost:5000/api/message/'+$scope.messageById).then(function(result){
+			$scope.result = result.data;
+			console.log(result.data);
+		});
+	}
+	document.getElementById("getById").addEventListener("click", getMessageById);
+
 	//post
 	function postMessage(){
 		$http.post('http://localhost:5000/api/message', {msg: $scope.message});
 		console.log("post");
-		getMessage();
+		
 	}
 	document.getElementById("post").addEventListener("click", postMessage);
 
+	//delete all messages
+	function deleteMessages(){
+		$http.delete('http://localhost:5000/api/message/').then(function(result){
+			console.log("delete messages", result.data);
+		});
+		
+	}
+
+	document.getElementById("delete").addEventListener("click", deleteMessages);
+
+	//delete by Id
+	function deleteMessageById(){
+		$http.delete('http://localhost:5000/api/message/'+$scope.messageId).then(function(result){
+			console.log("delete messages", result.data);
+		});
+		
+	}
+
+	document.getElementById("deleteById").addEventListener("click", deleteMessageById);
 	
 };
 
